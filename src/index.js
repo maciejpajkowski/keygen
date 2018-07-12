25b07e08f1801b147ffe9cd0b2558c01093d262b
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2018-07-12 12:36:49 +0200

    Added an option to create longer keys & slightly improved CSS

--- src/index.js
+++ src/index.js
@@ -4,22 +4,27 @@ const fghij = ["F", "G", "H", "I", "J"];
 const klmno = ["K", "L", "M", "N", "O"];
 const pqrst = ["P", "Q", "R", "S", "T"];
 const uvwxyz = ["U", "V", "W", "X", "Y", "Z"];
+const standardArr = [
+  ...numbers,
+  ...abcde,
+  ...numbers,
+  ...fghij,
+  ...numbers,
+  ...klmno,
+  ...numbers,
+  ...pqrst,
+  ...numbers,
+  ...uvwxyz,
+  ...numbers
+];
+
+const button = document.getElementById("app--generate-key");
+const inputs = document.getElementsByTagName("input");
+const generatedKey = document.getElementById("keyslot");
 
 const generateStandardKey = () => {
-  // standard key looks like this: AAAAA-BBBBB-CCCCC (17 characters)
-  const standardArr = [
-    ...numbers,
-    ...abcde,
-    ...numbers,
-    ...fghij,
-    ...numbers,
-    ...klmno,
-    ...numbers,
-    ...pqrst,
-    ...numbers,
-    ...uvwxyz,
-    ...numbers
-  ];
+  // standard key: AAAAA-BBBBB-CCCCC (17 characters)
+
   let key = [];
 
   for (let i = 0; i < 17; i++) {
@@ -32,10 +37,56 @@ const generateStandardKey = () => {
       key = key.join("");
     }
   }
-  const generatedKey = document.getElementById("keyslot");
   generatedKey.innerHTML = key;
   generatedKey.style.color = "black";
 };
 
-const button = document.getElementById("app--generate-key");
-button.addEventListener("click", generateStandardKey);
+const generate4Key = () => {
+  // 4 key: AAAAA-BBBBB-CCCCC-DDDDD (23 characters)
+
+  let key = [];
+
+  for (let i = 0; i < 23; i++) {
+    if (i === 5 || i === 11 || i === 17) {
+      key = [...key, "-"];
+    } else {
+      let rndNum = Math.floor(Math.random() * standardArr.length);
+      let char = standardArr[rndNum];
+      key = [...key, char];
+      key = key.join("");
+    }
+  }
+  generatedKey.innerHTML = key;
+  generatedKey.style.color = "black";
+};
+
+const generate5Key = () => {
+  // 5 key: AAAAA-BBBBB-CCCCC-DDDDD-EEEEE (29 characters)
+
+  let key = [];
+
+  for (let i = 0; i < 29; i++) {
+    if (i === 5 || i === 11 || i === 17 || i === 23) {
+      key = [...key, "-"];
+    } else {
+      let rndNum = Math.floor(Math.random() * standardArr.length);
+      let char = standardArr[rndNum];
+      key = [...key, char];
+      key = key.join("");
+    }
+  }
+  generatedKey.innerHTML = key;
+  generatedKey.style.color = "black";
+};
+
+const controller = () => {
+  if (inputs[0].checked === true) {
+    generateStandardKey();
+  } else if (inputs[1].checked === true) {
+    generate4Key();
+  } else if (inputs[2].checked === true) {
+    generate5Key();
+  }
+};
+
+button.addEventListener("click", controller);
